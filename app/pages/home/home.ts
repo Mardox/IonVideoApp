eb4e682ec1be069325bafa586366a5e2e0431633
kalyandechiraju <kalyandechiraju@outlook.com>
2016-07-04 21:50:25 +0530

    Add Image capture functionality

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,15 +1,26 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navController: NavController) {
+  public base64Image: string;
 
+  constructor(private navController: NavController) {
   }
 
   openCamera() {
-    
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        mediaType: Camera.MediaType.ALLMEDIA,
+        targetHeight: 1000,
+        targetWidth: 1000
+    }).then((videoData) => {
+        this.base64Image = "data:image/jpeg;base64," + videoData; 
+    }, (err) => {
+        console.log(err);
+    });
   }
 }
