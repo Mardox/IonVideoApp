142d6278cd94f45ac77a665811972bfe57e536c1
kalyandechiraju <kalyandechiraju@outlook.com>
2016-07-11 23:27:41 +0530

    temp checkin with lot of comments

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -7,20 +7,37 @@ declare var window: any;
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  public videoPath: any;
-  public thumbnailData: any;
+  public thumbnailData: string;
+  public base64Image: string;
 
   constructor(private navController: NavController) {
   }
 
   openCamera() {
     // capture callback
-    var captureSuccess = function (mediaFiles) {
-      var i, path, len, fail;
-      for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-        path = mediaFiles[i].fullPath;
+    //var context = this;
+    //var captureSuccess = function (mediaFiles) {
+      // var i, path, len;
+      // for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+      //   path = mediaFiles[i].fullPath;
+      //   alert(path);
+      //   navigator.createThumbnail(path, function (err, imageData) {
+      //     if (err)
+      //       throw err;
+
+      //     context.thumbnailData = "data:image/jpeg;base64," + imageData;
+      //     alert(context.thumbnailData);
+      //     var img = document.createElement("img");
+      //     img.src = context.thumbnailData;
+      //   });
+
+        // navigator.createThumbnail(path).then((imageData) => {
+        //   thumbnailData = "data:image/jpeg;base64," + imageData;
+        //   alert(thumbnailData);
+        // });
+
         // do something interesting with the file
-        var thumbnailPath = path.slice(0, -4) + '.png';
+        // var thumbnailPath = path.slice(0, -4) + '.png';
         // alert(thumbnailPath);
         // window.PKVideoThumbnail.createThumbnail(path, thumbnailPath).then((data) => {
         //   this.thumbnailData = data;
@@ -28,41 +45,55 @@ export class HomePage {
         //   alert(this.thumbnailData);
         // }).catch((err) => { });
 
-        window.PKVideoThumbnail.createThumbnail(path, thumbnailPath, function (prevSucc) {
-          return prevImageSuccess(prevSucc);
-        }, fail);
+        // window.PKVideoThumbnail.createThumbnail(path, thumbnailPath, function (prevSucc) {
+        //   return prevImageSuccess(prevSucc);
+        // }, fail);
 
-        function prevImageSuccess(succ) {
-          alert('success');
-        };
-      }
+        // function prevImageSuccess(succ) {
+        //   alert('success');
+        // };
+      //}
 
       // this.videoPath = mediaFiles.fullPath;
       // alert(this.videoPath);
       //this.createThumbnail();
       // console.log('videoPath: ' + this.videoPath);
-    };
+    //};
 
     // capture error callback
     var captureError = function (error) {
       navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
     };
     // start video capture
-    navigator.device.capture.captureVideo(captureSuccess, captureError, { limit: 1, duration: 20 });
+    navigator.device.capture.captureVideo(this.generateThumbnail, captureError, { limit: 1, duration: 10 });
   }
 
-  createThumbnail(path) {
-    alert('Inside thumbnail');
-    var options = {
-      mode: window.PKVideoThumbnail.options.mode.base64
+  generateThumbnail(mediaFiles) {
+    var i, path, len;
+    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+      path = mediaFiles[i].fullPath;
     }
-    var thumbnailPath = path.slice(0, -4) + '.png';
-    window.PKVideoThumbnail.createThumbnail(path, thumbnailPath, options).then((data) => {
-      this.thumbnailData = data;
-      console.log('thumbnail: ' + this.thumbnailData);
-      alert(this.thumbnailData);
-    });
+    var newPath = path.replace("file:/", "file:///");
+    window.PKVideoThumbnail.createThumbnail(newPath, "IGNORE",
+      { mode: "base64", quality: .8, position: 5.0, resize: { height: 384, width: 384 } }).then((data) => {
+        //alert(data);
+        this.base64Image = data;
+      }).catch((err) => {
+        alert(err);
+      });
   }
 
+  // createThumbnail(path) {
+  //   alert('Inside thumbnail');
+  //   var options = {
+  //     mode: window.PKVideoThumbnail.options.mode.base64
+  //   }
+  //   var thumbnailPath = path.slice(0, -4) + '.png';
+  //   window.PKVideoThumbnail.createThumbnail(path, thumbnailPath, options).then((data) => {
+  //     this.thumbnailData = data;
+  //     console.log('thumbnail: ' + this.thumbnailData);
+  //     alert(this.thumbnailData);
+  //   });
+  // }
 
 }
