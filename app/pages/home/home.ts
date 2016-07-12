efeb227072a6c512bb0b18f2565317f6fe3ca1d7
kalyandechiraju <kalyandechiraju@outlook.com>
2016-07-13 01:14:20 +0530

    Fix 'this' context. But data binding not working . May be because of nested callbacks, UI is not being updated. Temporarily push base64 data to a new Image tag created dynamically.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -5,33 +5,48 @@ declare var navigator: any;
 declare var window: any;
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  public thumbnailData: string;
-  constructor(private navController: NavController) {
-    //alert(this.thumbnailData);
-  }
-  openCamera() {
-    // capture error callback
-    var captureError = function (error) {
-      navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
-    };
-    // start video capture
-    navigator.device.capture.captureVideo(this.generateThumbnail, captureError, { limit: 1, duration: 10 });
-    
-  }
-  generateThumbnail(mediaFiles) {
-    var i, path, len;
-    for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-      path = mediaFiles[i].fullPath;
+    public thumbnail: string;
+    public base64Image: string;
+
+    constructor(private navController: NavController) {
     }
-    var newPath = path.replace("file:/", "file:///");
-    window.PKVideoThumbnail.createThumbnail(newPath, "IGNORE",
-      { mode: "base64", quality: .8, position: 5.0, resize: { height: 384, width: 384 } }).then((imageData) => {
-        this.thumbnailData = imageData;
-      }).catch((err) => {
-        alert(err);
-      });
-  }
+    takeVideo() {
+        // capture error callback
+        var captureError = function (error) {
+            alert('Error code: ' + error.code + ' Capture Error');
+        };
+        // start video capture
+        navigator.device.capture.captureVideo(this.generateThumbnail, captureError, { limit: 1, duration: 10 });
+    }
+    generateThumbnail = (mediaFiles) => {
+        var i, path, len;
+        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+            path = mediaFiles[i].fullPath;
+        }
+        var newPath = path.replace("file:/", "file:///");
+        window.PKVideoThumbnail.createThumbnail(newPath, "IGNORE", { mode: "base64", quality: .8, position: 5.0, resize: { height: 384, width: 384 } }).then((imageData) => {
+            this.thumbnail = imageData;
+            var img = document.createElement("img");
+            img.src = imageData;
+            document.getElementById("content").appendChild(img);
+        }).catch((err) => {
+            alert(err);
+        });
+    }
+
+    takePicture() {
+        Camera.getPicture({
+            destinationType: Camera.DestinationType.DATA_URL,
+            targetWidth: 1000,
+            targetHeight: 1000
+        }).then((imageData) => {
+            // imageData is a base64 encoded string
+            this.base64Image = "data:image/jpeg;base64," + imageData;
+        }, (err) => {
+            console.log(err);
+        });
+    }
 }
