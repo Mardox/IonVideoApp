5dd08fb12cfffd0d468e0b070a40115f8b25532b
kalyandechiraju <kalyandechiraju@outlook.com>
2016-07-15 01:19:01 +0530

    Add Parse S3 upload code for images

--- app/app.ts
+++ app/app.ts
@@ -1,22 +1,43 @@
-import {Component} from '@angular/core';
-import {Platform, ionicBootstrap} from 'ionic-angular';
-import {StatusBar} from 'ionic-native';
-import {HomePage} from './pages/home/home';
+import {Component} from "@angular/core";
+import {Platform, ionicBootstrap} from "ionic-angular";
+import {StatusBar} from "ionic-native";
+import {HomePage} from "./pages/home/home";
+declare function require(name: string);
+let Parse = require("parse/node");
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+    template: "<ion-nav [root]='rootPage'></ion-nav>"
 })
 export class MyApp {
-  rootPage: any = HomePage;
+    rootPage: any = HomePage;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-    });
-  }
+    constructor(platform: Platform) {
+        Parse.initialize("ion-video-app", "unused", "change-this-key");
+        Parse.serverURL = "http://parse-server-4282.herokuapp.com/parse";
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
+        });
+
+        // let testObjClass = new Parse.Object.extend("TestObject");
+        //
+        // // Create instance of testObjClass
+        // let testObj = new testObjClass();
+        //
+        // // Add data you want to save as a hush
+        // testObj.set("key1", "val1");
+        // testObj.set("key2", "val2");
+        // testObj.set("key3", "val3");
+        //
+        // // Save object to DB
+        // testObj.save().then((obj) => {
+        //     console.log("New object created with objectId: " + testObj.id);
+        // }, (err) => {
+        //     console.log("Failed to create new object, with error code: " + err.message);
+        // });
+    }
 }
 
 ionicBootstrap(MyApp);
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,13 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import {Camera} from 'ionic-native';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+import {Camera} from "ionic-native";
 declare var navigator: any;
 declare var window: any;
+declare function require(name: string);
+let Parse = require("parse/node");
 
 @Component({
-    templateUrl: 'build/pages/home/home.html'
+    templateUrl: "build/pages/home/home.html"
 })
 export class HomePage {
     public thumbnail: string;
@@ -15,23 +17,43 @@ export class HomePage {
     }
     takeVideo() {
         // capture error callback
-        var captureError = function (error) {
-            alert('Error code: ' + error.code + ' Capture Error');
+        let captureError = function(error) {
+            alert("Error code: " + error.code + " Capture Error");
         };
         // start video capture
         navigator.device.capture.captureVideo(this.generateThumbnail, captureError, { limit: 1, duration: 10 });
     }
     generateThumbnail = (mediaFiles) => {
-        var i, path, len;
+        let i: any, path, len;
         for (i = 0, len = mediaFiles.length; i < len; i += 1) {
             path = mediaFiles[i].fullPath;
         }
-        var newPath = path.replace("file:/", "file:///");
+
+        // upload video to parse in the background
+        // let file = new Parse.File("video-file", path);
+        // file.save().then(function() {
+        //     // The file has been saved to Parse.
+        //     console.log("File uploaded....");
+        // }, function(error) {
+        //     // The file either could not be read, or could not be saved to Parse.
+        //     console.log("File upload failed.");
+        // });
+
+        let newPath = path.replace("file:/", "file:///");
         window.PKVideoThumbnail.createThumbnail(newPath, "IGNORE", { mode: "base64", quality: .8, position: 5.0, resize: { height: 384, width: 384 } }).then((imageData) => {
             this.thumbnail = imageData;
-            var img = document.createElement("img");
+            let img = document.createElement("img");
             img.src = imageData;
             document.getElementById("content").appendChild(img);
+
+            let file = new Parse.File("thumbnail-file", {base64: imageData});
+            file.save().then(function() {
+                // The file has been saved to Parse.
+                console.log("File uploaded....");
+            }, function(error) {
+                // The file either could not be read, or could not be saved to Parse.
+                console.log("File upload failed.");
+            });
         }).catch((err) => {
             alert(err);
         });
@@ -45,6 +67,16 @@ export class HomePage {
         }).then((imageData) => {
             // imageData is a base64 encoded string
             this.base64Image = "data:image/jpeg;base64," + imageData;
+            let file = new Parse.File("thumbnail-file", {base64: this.base64Image});
+            file.save().then(function() {
+                // The file has been saved to Parse.
+                console.log("File uploaded....");
+                alert("uploaded");
+            }, function(error) {
+                // The file either could not be read, or could not be saved to Parse.
+                console.log("File upload failed.");
+                alert(error.message);
+            });
         }, (err) => {
             console.log(err);
         });
