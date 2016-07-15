57a910d27866e89078cc09a16f6a7948393b2acb
kalyandechiraju <kalyandechiraju@outlook.com>
2016-07-16 00:43:31 +0530

    Add video upload code. But getting error while read file

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -28,35 +28,70 @@ export class HomePage {
         for (i = 0, len = mediaFiles.length; i < len; i += 1) {
             path = mediaFiles[i].fullPath;
         }
-
-        // upload video to parse in the background
-        // let file = new Parse.File("video-file", path);
-        // file.save().then(function() {
-        //     // The file has been saved to Parse.
-        //     console.log("File uploaded....");
-        // }, function(error) {
-        //     // The file either could not be read, or could not be saved to Parse.
-        //     console.log("File upload failed.");
-        // });
-
         let newPath = path.replace("file:/", "file:///");
+
+        // let b64toBlobAlt = function(dataURI, contentType) {
+        //     let ab, byteString, i, ia;
+        //     byteString = atob(dataURI.split(",")[1]);
+        //     ab = new ArrayBuffer(byteString.length);
+        //     ia = new Uint8Array(ab);
+        //     i = 0;
+        //     while (i < byteString.length) {
+        //         ia[i] = byteString.charCodeAt(i);
+        //         i++;
+        //     }
+        //     return new Blob([ab], {
+        //         type: contentType
+        //     });
+        // };
+
+        window.resolveLocalFileSystemURL(path, function(fileEntry) {
+            return fileEntry.file(function(data) {
+                // alert("Data : " + data);
+                let reader = new FileReader();
+                reader.onloadend = function(e: any) {
+                    try {
+                        // alert(this.result);
+                        let videoFile = new Parse.File("video-file", { base64: this.result });
+                        alert("video upload begin");
+                        videoFile.save().then(() => {
+                            // The file has been saved to Parse.
+                            console.log("File uploaded....");
+                            alert("video uploaded");
+                        }, (error) => {
+                            // The file either could not be read, or could not be saved to Parse.
+                            console.log("File upload failed.");
+                        });
+                    } catch (err) {
+                        alert(err.message);
+                    }
+                };
+                return reader.readAsDataURL(data);
+            });
+        });
+
         window.PKVideoThumbnail.createThumbnail(newPath, "IGNORE", { mode: "base64", quality: .8, position: 5.0, resize: { height: 384, width: 384 } }).then((imageData) => {
             this.thumbnail = imageData;
             let img = document.createElement("img");
             img.src = imageData;
             document.getElementById("content").appendChild(img);
 
-            let file = new Parse.File("thumbnail-file", {base64: imageData});
-            file.save().then(function() {
+            let file = new Parse.File("thumbnail-file", { base64: imageData });
+            file.save().then(() => {
                 // The file has been saved to Parse.
                 console.log("File uploaded....");
-            }, function(error) {
+                alert("uploaded");
+            }, (error) => {
                 // The file either could not be read, or could not be saved to Parse.
                 console.log("File upload failed.");
             });
         }).catch((err) => {
             alert(err);
         });
+
+        // upload video to parse in the background
+        // let parseFile = newPath.replace("file:///", "/");
+        // alert(parseFile);
     }
 
     takePicture() {
@@ -67,12 +102,12 @@ export class HomePage {
         }).then((imageData) => {
             // imageData is a base64 encoded string
             this.base64Image = "data:image/jpeg;base64," + imageData;
-            let file = new Parse.File("thumbnail-file", {base64: this.base64Image});
-            file.save().then(function() {
+            let file = new Parse.File("thumbnail-file", { base64: this.base64Image });
+            file.save().then(() => {
                 // The file has been saved to Parse.
                 console.log("File uploaded....");
                 alert("uploaded");
-            }, function(error) {
+            }, (error) => {
                 // The file either could not be read, or could not be saved to Parse.
                 console.log("File upload failed.");
                 alert(error.message);
